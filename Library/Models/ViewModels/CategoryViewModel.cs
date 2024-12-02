61071344b27be532e7a97459b44f64669754426f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-12-02 16:36:47 +0200

    Delete Library directory

--- Library/Models/ViewModels/CategoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Library.Models.ViewModels
-{
-    public class CategoryViewModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; } = string.Empty;
-    }
-}
